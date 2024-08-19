f3e55032cc8f5e165fe3ee25e4a3b845c8f0c4ec
Robert MacLeod <sarca571ca@gmail.com>
2024-08-19 19:52:21 +0800

    removed var

--- letter_count.py
+++ letter_count.py
@@ -6,11 +6,10 @@ msg = msg.lower()
 msg_count = {}
 
 for c in msg:
-    if c in alphabet:
-        if c in msg_count:
-            msg_count[c] += 1
-        else:
-            msg_count[c] = 1
+    if c in msg_count:
+        msg_count[c] += 1
+    else:
+        msg_count[c] = 1
 
 for key in msg_count:
     print("Key: ", key, " Value: ", msg_count[key])
